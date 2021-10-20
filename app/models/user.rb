6ff631728534c5f11db7c7723112ec873d72bf3d
Jarret Nachtigal <jarretn1224@gmail.com>
2021-10-20 15:30:46 -0400

    more validation fiddlings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
+  # validates :email, format: { with: , message: "invalid email"}
 
 end
